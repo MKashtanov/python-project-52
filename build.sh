b2aace1e3f3057aa9f85cf713d8177cbb61ea510
MKashtanov <MKashtanov@mail.ru>
2024-05-17 09:55:57 +0400

    add build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry install
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+cd task_manager
+django-admin compilemessages
