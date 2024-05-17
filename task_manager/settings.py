62fb6276d94eb6d7953f7602fcc6e31e1da6ffd7
MKashtanov <MKashtanov@mail.ru>
2024-05-17 10:23:06 +0400

    fix ALLOWED_HOSTS in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -35,9 +35,10 @@ ALLOWED_HOSTS = [
     'webserver',
     '127.0.0.1',
     'localhost',
-    '.render.com',
 ]
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
